ff19e0d5cf2b25703c3d5deff08a603b922df64e
jocontreras <jen.almarasy@gmail.com>
2017-03-22 21:56:32 +0100

    transfer cooper_client to new repo

---
+++ www/js/controllers.js
@@ -0,0 +1,58 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('TestController', function($scope) {
+  $scope.gender = ['Male', 'Female']
+  $scope.ageValues = {
+    min: 20,
+    max: 60,
+    value: 20
+  };
+  $scope.distanceValues = {
+    min: 1000,
+    max: 3500,
+    value: 1000
+  };
+  $scope.data = {};
+  $scope.calculateCooper = function() {
+    var person = new Person({
+      gender: $scope.data.gender,
+      age: $scope.data.age
+    });
+    person.assessCooper($scope.data.distance);
+    $scope.person = person;
+    console.log($scope.person)
+  };
+});
